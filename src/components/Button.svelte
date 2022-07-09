6934c43433fcc2af302e823f65a99a4db306b24d
Rishab Garg <rishgarg@gmail.com>
2022-07-09 21:29:29 +0200

    feat!: initial commit - based on portfolio template from @astrojs - modified to use svelte - replaced names - added commit linting - added github pages script

---
+++ src/components/Button.svelte
@@ -0,0 +1,13 @@
+<span class="button">
+  <slot />
+</span>
+
+<style lang="scss">
+  .button {
+    display: inline-block;
+    border: 3px solid currentColor;
+    padding: 0.5em 1em;
+    font-weight: 700;
+    text-transform: uppercase;
+  }
+</style>
