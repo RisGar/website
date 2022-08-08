accd3f9e0c482f0b595041da378443c3cea53356
Rishab Garg <rishgarg@gmail.com>
2022-08-08 19:33:20 +0200

    feat: switched from gh pages to netlify

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,9 +1,11 @@
 import type { UserConfig } from "@commitlint/types"
 
-export const Configuration: UserConfig = {
+const Configuration: UserConfig = {
   /*
    * Resolve and load @commitlint/config-conventional from node_modules.
    * Referenced packages must be installed
    */
   extends: ["@commitlint/config-conventional"],
 }
+
+export default Configuration
