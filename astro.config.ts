83a4e0dd2a28a3cc24e4464415223871c57dec27
Rishab Garg <rishgarg@gmail.com>
2022-08-08 18:30:46 +0200

    feat: remove pwa
    + small tweaks

--- astro.config.ts
+++ astro.config.ts
@@ -1,43 +1,14 @@
 import { defineConfig } from "astro/config"
 import svelte from "@astrojs/svelte"
 import tailwind from "@astrojs/tailwind"
-import { VitePWA } from "vite-plugin-pwa"
-
-import { metadata } from "./src/components/defaults"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     svelte(),
     tailwind({
-      config: { applyBaseStyles: true },
+      config: { applyBaseStyles: false },
     }),
   ],
-  vite: {
-    plugins: [
-      VitePWA({
-        manifest: {
-          name: metadata.title,
-          short_name: metadata.short_title,
-          description: metadata.description,
-          theme_color: "#da532c",
-          icons: [
-            {
-              src: "/android-chrome-192x192.png",
-              sizes: "192x192",
-              type: "image/png",
-            },
-            {
-              src: "/android-chrome-512x512.png",
-              sizes: "512x512",
-              type: "image/png",
-            },
-          ],
-          background_color: "#da532c",
-          display: "standalone",
-        },
-      }),
-    ],
-  },
   site: "https://rishab-garg.me/",
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     colors: {
       transparent: "transparent",
