501009f24953aa77d7d98e59fa460c4ee6310dee
Rishab Garg <rishgarg@gmail.com>
2023-02-19 11:38:00 +0100

    feat: added service worker

--- astro.config.ts
+++ astro.config.ts
@@ -1,23 +1,32 @@
-import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
-import robotsTxt from "astro-robots-txt";
-import sitemap from "@astrojs/sitemap";
-import image from "@astrojs/image";
-import prefetch from "@astrojs/prefetch";
-
-import mdx from "@astrojs/mdx";
+import { defineConfig } from "astro/config"
+import svelte from "@astrojs/svelte"
+import tailwind from "@astrojs/tailwind"
+import robotsTxt from "astro-robots-txt"
+import sitemap from "@astrojs/sitemap"
+import image from "@astrojs/image"
+import prefetch from "@astrojs/prefetch"
+import serviceWorker from "astrojs-service-worker"
+import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), robotsTxt(), sitemap(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), prefetch({
-    throttle: 5
-  }), mdx()],
-  site: "https://rishab-garg.me"
-});
+  integrations: [
+    svelte(),
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    robotsTxt(),
+    sitemap(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    prefetch({
+      throttle: 5,
+    }),
+    mdx(),
+    serviceWorker(),
+  ],
+  site: "https://rishab-garg.me",
+})
