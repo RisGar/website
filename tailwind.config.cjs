2c552e29360beb759cf8827bba0a3dadb44bd744
Rishab Garg <me@rishab-garg.me>
2023-11-13 20:12:56 +0100

    feat!: removed most features

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      black: "#282c34",
-      white: "#abb2bf",
-      "light-red": "#e06c75",
-      "dark-red": "#be5046",
-      green: "#98c379",
-      "light-yellow": "#e5c07b",
-      "dark-yellow": "#d19a66",
-      blue: "#61afef",
-      magenta: "#c678dd",
-      cyan: "#56b6c2",
-      "gutter-grey": "#4b5263",
-      "comment-grey": "#5c6370",
+    extend: {
+      colors: {
+        // transparent: "transparent",
+        // current: "currentColor",
+        "light-red": "#e06c75",
+        "dark-red": "#be5046",
+        green: "#98c379",
+        "light-yellow": "#e5c07b",
+        "dark-yellow": "#d19a66",
+        blue: "#61afef",
+        magenta: "#c678dd",
+        cyan: "#56b6c2",
+        "gutter-grey": "#4b5263",
+        "comment-grey": "#5c6370",
+      },
     },
     fontFamily: {
       sans: [
@@ -34,12 +35,7 @@ module.exports = {
         "sans-serif",
       ],
     },
-    transitionDuration: {
-      DEFAULT: "200ms",
-    },
-    transitionTimingFunction: {
-      DEFAULT: "linear",
-    },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography")],
 }
