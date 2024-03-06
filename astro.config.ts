76e2defc6dbd0287ffb4998090b9d3677dfecdaf
Rishab Garg <me@rishab-garg.me>
2024-03-06 07:10:08 +0100

    feat: changed style to light mode

--- astro.config.ts
+++ astro.config.ts
@@ -3,16 +3,21 @@ import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
 import sitemap from "@astrojs/sitemap"
 import serviceWorker from "astrojs-service-worker"
+import compress from "astro-compress"
 
+// https://astro.build/config
 export default defineConfig({
   prefetch: {
     prefetchAll: true,
   },
   integrations: [
-    tailwind({ applyBaseStyles: false }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
     robotsTxt(),
     sitemap(),
     serviceWorker(),
+    compress(),
   ],
   site: "https://rishab-garg.me",
 })
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -29,7 +29,7 @@ module.exports = {
       },
     },
     fontFamily: {
-      sans: ["Inter", "sans-serif"],
+      sans: ["Lato", "sans-serif"],
       serif: ["DM Serif Display", "serif"],
     },
   },
