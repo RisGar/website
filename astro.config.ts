3a5ae5a1e4021d441074e976bb74d4d37bd00e4d
Rishab Garg <me@rishab-garg.me>
2024-03-06 07:26:41 +0100

    fix: remove useless service worker

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
 import sitemap from "@astrojs/sitemap"
-import serviceWorker from "astrojs-service-worker"
 import compress from "astro-compress"
 
 import vercel from "@astrojs/vercel/static"
@@ -18,7 +17,6 @@ export default defineConfig({
     }),
     robotsTxt(),
     sitemap(),
-    serviceWorker(),
     compress(),
   ],
   site: "https://rishab-garg.me",
